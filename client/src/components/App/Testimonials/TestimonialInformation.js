7e2784b5175fc335c7eab96db25e5c02a870734f
Samudra Perera <samudrapup@gmail.com>
2023-04-03 16:23:22 -0400

    Created the testimonial component, adding emailForm component

---
+++ client/src/components/App/Testimonials/TestimonialInformation.js
@@ -0,0 +1,30 @@
+export const Testimonials = [
+  {
+    name: "Jus Reign",
+    span: "Youtuber, JusReign, 300 Million Subscribers",
+    quote:
+      "With Cleancut, we were able to review the scene we had just shot and make needed edits on the day of production, this even helped with the final edit making everything run faster.",
+    avatarURL: "#",
+  },
+  {
+    name: "Jus Reign",
+    span: "Youtuber, JusReign, 300 Million Subscribers",
+    quote:
+      "With Cleancut, we were able to review the scene we had just shot and make needed edits on the day of production, this even helped with the final edit making everything run faster.",
+    avatarURL: "#",
+  },
+  {
+    name: "Jus Reign",
+    span: "Youtuber, JusReign, 300 Million Subscribers",
+    quote:
+      "With Cleancut, we were able to review the scene we had just shot and make needed edits on the day of production, this even helped with the final edit making everything run faster.",
+    avatarURL: "#",
+  },
+  {
+    name: "Jus Reign",
+    span: "Youtuber, JusReign, 300 Million Subscribers",
+    quote:
+      "With Cleancut, we were able to review the scene we had just shot and make needed edits on the day of production, this even helped with the final edit making everything run faster.",
+    avatarURL: "#",
+  },
+];
---
+++ client/src/components/SharedComponents/Footer/EmailForm.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import styled from "styled-components";
+
+const EmailForm = () => {
+  return;
+  <div>EmailForm</div>;
+};
+
+export default EmailForm;
