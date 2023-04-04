1daeb070ed2cf0d1c7658d38f0d5f254e4889d4e
Samudra Perera <samudrapup@gmail.com>
2023-04-04 02:59:01 -0400

    Completed Testimoninal Card Component

--- client/src/components/App/Testimonials/style/TestimonialCard.style.js
+++ client/src/components/App/Testimonials/style/TestimonialCard.style.js
@@ -2,40 +2,54 @@ import styled from "styled-components";
 import { StyledParagraph } from "../../CallToAction/styles/CTA.styled";
 
 const CardContainer = styled.div`
-  width: 40%;
+  width: 35%;
   background-color: white;
-  margin: 1rem;
-  padding: 1rem;
+  margin: 1.5rem;
+  padding: 1.5rem;
   border-radius: 1rem;
 `;
 
 const CardQuote = styled(StyledParagraph)`
-  font-size: 1rem;
+  font-size: 1.1rem;
+  width: 90%;
+  line-height: 1.4;
+  padding-top: .5rem;
 `;
 
 const AvatarContainer = styled.div`
-    display: flex;
-    flex-direction: row;
+  display: flex;
+  flex-direction: row;
+  padding: 2rem 0 1.5rem 0;
 `;
 
 const Avatar = styled.img`
-border-radius: 50%;
-height: 90px;
-width: auto;
+  border-radius: 50%;
+  height: 90px;
+  width: auto;
 `;
 
 const AvatarName = styled.span`
-
-`
+  font-family: Graphik-500-italic;
+  font-size: .9rem;
+`;
 const AvatarSpan = styled.span`
-    font-family: Graphik-300-italic;
-`
-
+  font-family: Graphik-300-italic;
+  font-size: .9rem;
+`;
 
 const AvatarTextContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 1rem;
+`;
 
-export { CardContainer, CardQuote, AvatarContainer, Avatar, AvatarTextContainer, AvatarSpan, AvatarName };
+export {
+  CardContainer,
+  CardQuote,
+  AvatarContainer,
+  Avatar,
+  AvatarTextContainer,
+  AvatarSpan,
+  AvatarName,
+};
